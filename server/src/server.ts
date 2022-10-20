69776ab4a6a7d29a7e597a5d323b6037202d32c0
ThiagoLG <thiago.lg53@gmail.com>
2022-10-20 01:49:13 -0300

    Criação da estrutura do servidor para montar as entidades de banco + implementação dos gets + inciado criação da operação de post (em andamento)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,82 @@
-import Express from "express"; 
+import Express, { request, response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 /*- Criação do app com base no express -*/
 const app = Express();
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+app.use(Express.json());
 
-/*- Configuração de rota para a url "/ads" -*/
-app.get('/ads', (request, response) => {
+/*- Get ads -*/
+app.get("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
 
-    return response.json([
-        { id: 1, name: 'Anúncio 1' },
-        { id: 2, name: 'Anúncio 2' },
-        { id: 3, name: 'Anúncio 3' },
-        { id: 4, name: 'Anúncio 4' },
-    ]);
+/*- Get games -*/
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return response.json(games);
+});
 
-})
+/*- Get Ads by game-*/
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourEnd: true,
+      hourStart: true,
+    },
+    where: {
+      gameId: gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return response.json(
+    ads.map((ad) => ({ ...ad, weekDays: ad.weekDays.split(",") }))
+  );
+});
+
+/*- Get discord by ad -*/
+app.get("/ads/:id/discord", async (request, response) => {
+  const adId = request.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    where: {
+      id: adId,
+    },
+    select: {
+      discord: true,
+    },
+  });
+
+  return response.json({ discord: ad.discord });
+});
+
+/*- Creat new Ad -*/
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+
+  //TODO -> Finalizar a implementação da inclusão de novos ads no banco
+
+  return response.json(request.body);
+});
 
 /*- Ativa o listener para a porta 3333 -*/
 app.listen(3333);
